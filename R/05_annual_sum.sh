1807d24aa6c183a074b2dffcc9908e0c61ba30fb
Stephen Huysman <shuysman@gmail.com>
2024-07-27 23:17:36 -0600

    allow batching sites

--- R/05_annual_sum.sh
+++ R/05_annual_sum.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 set -euxo pipefail
 
-export in_dir="${HOME}/out/surprise/collated"
-export out_dir="${HOME}/out/surprise/sums"
+
 
 if [ ! -d $out_dir ]; then
     mkdir $out_dir    
 fi
 
 vars="AET Deficit"
+
 #models="bcc-csm1-1-m bcc-csm1-1 BNU-ESM CanESM2 CNRM-CM5 CSIRO-Mk3-6-0 GFDL-ESM2G GFDL-ESM2M HadGEM2-CC365 HadGEM2-ES365 inmcm4 IPSL-CM5A-LR IPSL-CM5A-MR IPSL-CM5B-LR MIROC5 MIROC-ESM-CHEM MIROC-ESM MRI-CGCM3 NorESM1-M"
-#scenarios="rcp45 rcp85"
+# models="CanESM2 HadGEM2-CC365 MRI-CGCM3"
+# scenarios="rcp45 rcp85"
 
 models="historical"
 scenarios="gridmet"
+
+sites="avalanche surprise static_west static_east"
 
 calc_annual_sum () {
     model=$1
     scenario=$2
     var=$3
+    site=$4
+    echo $site $model $scenario $var
 
-    echo $model $scenario $var
+    in_dir="${HOME}/out/${site}/collated"
+    out_dir="${HOME}/out/${site}/sums"
+    #in_dir="/media/smithers/shuysman/data/out/nps-wb/${site}/projections/collated"
+    #out_dir="/media/smithers/shuysman/data/out/nps-wb/${site}/projections/sums"
     
     for file in ${in_dir}/${model}_${scenario}_${var}_*.nc; do
 	cdo yearsum $file "${file}_sum.nc"
@@ -31,4 +39,4 @@ calc_annual_sum () {
 
 export -f calc_annual_sum
 
-parallel -j 2 calc_annual_sum {} ::: $models ::: $scenarios ::: $vars
+parallel -j 4 calc_annual_sum {} ::: $models ::: $scenarios ::: $vars ::: $sites
