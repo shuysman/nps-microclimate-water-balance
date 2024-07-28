66d3e7520299940a812b14720bcbae8fe06e1bac
Stephen Huysman <shuysman@gmail.com>
2024-07-27 23:18:21 -0600

    put directory check in right location

--- R/05_annual_sum.sh
+++ R/05_annual_sum.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-
-
-if [ ! -d $out_dir ]; then
-    mkdir $out_dir    
-fi
-
 vars="AET Deficit"
 
 #models="bcc-csm1-1-m bcc-csm1-1 BNU-ESM CanESM2 CNRM-CM5 CSIRO-Mk3-6-0 GFDL-ESM2G GFDL-ESM2M HadGEM2-CC365 HadGEM2-ES365 inmcm4 IPSL-CM5A-LR IPSL-CM5A-MR IPSL-CM5B-LR MIROC5 MIROC-ESM-CHEM MIROC-ESM MRI-CGCM3 NorESM1-M"
@@ -29,7 +23,12 @@ calc_annual_sum () {
     out_dir="${HOME}/out/${site}/sums"
     #in_dir="/media/smithers/shuysman/data/out/nps-wb/${site}/projections/collated"
     #out_dir="/media/smithers/shuysman/data/out/nps-wb/${site}/projections/sums"
+
     
+    if [ ! -d $out_dir ]; then
+	mkdir $out_dir    
+    fi
+
     for file in ${in_dir}/${model}_${scenario}_${var}_*.nc; do
 	cdo yearsum $file "${file}_sum.nc"
     done
