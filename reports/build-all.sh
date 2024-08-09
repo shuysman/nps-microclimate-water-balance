84bf00411f8fadac2ab97885ac2366038e48b530
Stephen Huysman <shuysman@gmail.com>
2024-08-09 14:32:46 -0600

    build in parallel

--- reports/build-all.sh
+++ reports/build-all.sh
@@ -5,9 +5,15 @@ set -u
 set -x
 set -o pipefail
 
-for file in *.Rmd; do
-    Rscript -e "rmarkdown::render('$file')"
-done
+
+parallel -j 3 Rscript -e "rmarkdown::render\(\'{}\'\)" ::: *.Rmd
+
+scp -B burroughs.html web:/www/data/research/burroughs/index.html
+scp -B avalanche.html web:/www/data/research/core_areas/avalanche/index.html
+scp -B static_east.html web:/www/data/research/core_areas/static_east/index.html
+scp -B static_west.html web:/www/data/research/core_areas/static_west/index.html
+scp -B surprise.html web:/www/data/research/core_areas/surprise/index.html
+scp -B holly_lake_small.html web:/www/data/research/core_areas/holly_lake_small/index.html
 
 cd ../for_libby/
 rm grte_yell_wb.zip
