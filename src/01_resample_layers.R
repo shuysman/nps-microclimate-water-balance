891539efe4d755c60e25e7db01cc0d4b35e6815a
Stephen Huysman <shuysman@gmail.com>
2025-06-12 17:04:33 -0600

    generalize and set up batching

--- src/01_resample_layers.R
+++ src/01_resample_layers.R
@@ -3,48 +3,51 @@ library(sf)
 library(terra)
 library(glue)
 
-site <- "holly_lake_small"
+sites <- c("avalanche_peak", "chittenden", "cub_creek")
 
-data_dir <- file.path(glue("../data/input/{site}/"))
+for (site in sites) {
+  data_dir <- file.path(glue("../data/input/{site}/"))
 
-reference <- rast(file.path(data_dir, "dem/dem_nad83.tif"))
-
-dayl <- rast(file.path("../data/1980_dayl_na.nc4"))
-dayl2 <- project(dayl, reference, method = "near")
-dayl2 <- resample(dayl2, reference, method = "near")
-dayl2 <- crop(dayl2, reference)
-
-writeCDF(dayl2, file.path(data_dir, glue("1980_dayl_resampled.nc4")), compression = 9, overwrite = TRUE)
-
-t50 <- rast(file.path("../data/merged_jennings2.tif"))
-t50 <- project(subset(t50, 1), reference, method = "near")
-t50 <- resample(t50, reference, method = "near")
-t50 <- crop(t50, reference)
-
-writeRaster(t50, file.path(data_dir, "jennings_t50_coefficients.tif"), overwrite=TRUE)
+  reference <- rast(file.path(data_dir, "dem/dem_nad83.tif"))
 
 
-# soils <- st_read("/home/steve/OneDrive/core_areas/data/StephenHuysman_GRTE_WBP_ModelingAreas/soil/grte_modelingareas_ssurgo.gpkg") %>% vect
+  ### I don't think 1980_dayl_na.nc4 is needed anymore. It was used with Penman calculations using Daymet climate data.
+  ## dayl <- rast(file.path("../data/1980_dayl_na.nc4"))
+  ## dayl2 <- project(dayl, reference, method = "near")
+  ## dayl2 <- resample(dayl2, reference, method = "near")
+  ## dayl2 <- crop(dayl2, reference)
 
-soils <- st_read(file.path("../data/grte_modelingareas_ssurgo.gpkg")) %>% vect()
+  ## writeCDF(dayl2, file.path(data_dir, glue("1980_dayl_resampled.nc4")), compression = 9, overwrite = TRUE)
 
-### The curve number generator ssurgo download tool in QGIS returns strings for soil AWS.
-### R loads these as factors, and when multiplied by 10, you get the index of the string * 10 instead of
-### the AWS * 10.  This burned me and I lost a lot of time rerunning data.  Make sure that the
-### soil AWS values are numeric and then multiply by ten to convert cm to mm.
-soils$aws025wta <- as.numeric(soils$aws025wta) * 10 
-soils$aws050wta <- as.numeric(soils$aws050wta) * 10
-soils$aws0100wta <- as.numeric(soils$aws0100wta) * 10
-soils$aws0150wta <- as.numeric(soils$aws0150wta) * 10
+  t50 <- rast(file.path("../data/merged_jennings2.tif"))
+  t50 <- project(subset(t50, 1), reference, method = "near")
+  t50 <- resample(t50, reference, method = "near")
+  t50 <- crop(t50, reference)
+
+  writeRaster(t50, file.path(data_dir, "jennings_t50_coefficients.tif"), overwrite=TRUE)
 
 
-soils_025 <- soils %>%
+  # soils <- st_read("/home/steve/OneDrive/core_areas/data/StephenHuysman_GRTE_WBP_ModelingAreas/soil/grte_modelingareas_ssurgo.gpkg") %>% vect
+
+  soils <- st_read(file.path(data_dir, "soil/ssurgo.gpkg")) %>% vect()
+
+  ### The curve number generator ssurgo download tool in QGIS returns strings for soil AWS.
+  ### R loads these as factors, and when multiplied by 10, you get the index of the string * 10 instead of
+  ### the AWS * 10.  This burned me and I lost a lot of time rerunning data.  Make sure that the
+  ### soil AWS values are numeric and then multiply by ten to convert cm to mm.
+  soils$aws025wta <- as.numeric(soils$aws025wta) * 10 
+  soils$aws050wta <- as.numeric(soils$aws050wta) * 10
+  soils$aws0100wta <- as.numeric(soils$aws0100wta) * 10
+  soils$aws0150wta <- as.numeric(soils$aws0150wta) * 10
+
+
+  soils_025 <- soils %>%
     rasterize(reference, field = "aws025wta") 
 
-writeRaster(soils_025, file.path(data_dir, "soil/soil_whc_025.tif"), overwrite=TRUE)
+  writeRaster(soils_025, file.path(data_dir, "soil/soil_whc_025.tif"), overwrite=TRUE)
 
-soils_100 <- soils %>%
+  soils_100 <- soils %>%
     rasterize(reference, field = "aws0100wta")
 
-writeRaster(soils_100, file.path(data_dir, "soil/soil_whc_100.tif"), overwrite=TRUE)
-
+  writeRaster(soils_100, file.path(data_dir, "soil/soil_whc_100.tif"), overwrite=TRUE)
+}
