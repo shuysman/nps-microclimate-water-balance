2ab6922c1f8364b0c94eaa835fcd21480617f193
steve <steve@ecol-laptop.montana.edu>
2025-07-23 14:54:12 -0400

    fix formatting

--- src/00_clim_data.R
+++ src/00_clim_data.R
@@ -3,7 +3,7 @@ library(climateR)
 library(glue)
 library(tidyverse)
 
-args <- commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 
 site_name <- args[1]
 
@@ -11,26 +11,24 @@ site_data_path <- file.path(glue("../data/input/{site_name}/"))
 
 site_data <- read_csv("sites.csv") %>% filter(site == site_name)
 
-lon = site_data$lon
-lat = site_data$lat
+lon <- site_data$lon
+lat <- site_data$lat
 
 point <- data.frame(lon = lon, lat = lat) %>%
   vect(geom = c("lon", "lat"), crs = "EPSG:4326")
 
 gridmet_vars <- c("tmmn", "tmmx", "pr")
 gridmet_data <- getGridMET(point,
-                           varname = gridmet_vars,
-                           startDate = ymd("1979-01-01"),
-                           endDate = ymd("2024-12-31"),
-                           verbose = TRUE)
-
-
-
+  varname = gridmet_vars,
+  startDate = ymd("1979-01-01"),
+  endDate = ymd("2024-12-31"),
+  verbose = TRUE
+)
 
 models <- c(
-        "CanESM2",
-        "HadGEM2-CC365",
-        "MRI-CGCM3"
+  "CanESM2",
+  "HadGEM2-CC365",
+  "MRI-CGCM3"
 )
 scenarios <- c("rcp45", "rcp85")
 maca_vars <- c("tasmin", "tasmax", "pr")
@@ -40,8 +38,6 @@ maca_data <- getMACA(
   timeRes = "day",
   model = models,
   scenario = scenarios,
-  #startDate = ymd("2006-01-01"),
-  #endDate = ymd("2099-12-31"),
   startDate = ymd("2006-01-01"),
   endDate = ymd("2099-12-31"),
   verbose = TRUE
