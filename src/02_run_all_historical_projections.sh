a977f1401649552d816b39cd614fd9e3368f7111
Stephen Huysman <shuysman@gmail.com>
2026-01-16 12:04:24 -0700

    run relative to project root

--- src/02_run_all_historical_projections.sh
+++ src/02_run_all_historical_projections.sh
@@ -8,7 +8,7 @@ sites=$(awk -F, 'NR > 1 { print $1 }' sites.csv)
 ## “Divergent, Plausible, and Relevant Climate Futures for near- and Long-Term Resource Planning.”
 ## Climatic Change 167 (3): 38. https://doi.org/10.1007/s10584-021-03169-y.
 
-parallel -j32 python 02_start_wb_v_1_5.py {1} {2} {3} \
+parallel -j32 python src/02_start_wb_v_1_5.py {1} {2} {3} \
   ::: historical CanESM2 HadGEM2-CC365 MRI-CGCM3 MRI-CGCM3 \
   :::+ gridmet rcp85 rcp85 rcp45 rcp85 \
   ::: $sites
